cca42f63eeb38beede0ddaec06294ea4e8cc21e9
jaroslaw-filipiak <info@j-filipiak.pl>
2021-03-24 12:57:45 +0100

    pages to netlify

---
+++ pages/Apply-form.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="job-apply-container">
+    <BackToOffers />
+    <div class="single-job-content-wrapper">
+      <ApplyForm />
+      <ApplyFormSidebar />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "job-apply",
+  async asyncData({ params }) {
+    const slug = params.slug; // When calling /abc the slug will be "abc"
+    return { slug };
+  },
+  head() {
+    return {
+      bodyAttrs: {
+        class: "job-apply"
+      }
+    };
+  }
+};
+</script>
