4f61c1c80ee8781ae0ce22d130748aac7f42cea5
jaroslaw-filipiak <info@j-filipiak.pl>
2021-03-30 19:53:59 +0200

    sendEmail

--- components/Careers/ApplyForm.vue
+++ components/Careers/ApplyForm.vue
@@ -206,7 +206,7 @@
         <div>
           <BackButton />
         </div>
-        <div>
+        <div @click="sendEmail">
           <ApplyButton />
         </div>
       </div>
@@ -216,7 +216,6 @@
 
 <script>
 // require("dotenv").config();
-import axios from "axios";
 
 export default {
   props: ["title"],
@@ -231,102 +230,50 @@ export default {
       languages: ""
     };
   },
-  mounted() {
-    // this.initMailer();
-    // this.testMailer();
-    this.postMailer();
-  },
+  mounted() {},
   methods: {
-    //     SMTP username: Your Gmail address.
-    // SMTP password: Your Gmail password.
-    // SMTP server address: smtp.gmail.com.
-    // Gmail SMTP port (TLS): 587.
-    // SMTP port (SSL): 465.
-    // SMTP TLS/SSL required: yes.
+    sendEmail: function() {
+      console.log("send email");
+      // transporter
+      nodemailer.createTestAccount((err, account) => {
+        if (err) {
+          console.error("Failed to create a testing account. " + err.message);
+          return process.exit(1);
+        }
 
-    // initMailer() {
-    //   // 1
-    //   let transporter = nodemailer.createTransport({
-    //     service: "gmail",
-    //     auth: {
-    //       user: process.env.SMTP_USERNAME,
-    //       pass: process.env.SMTP_PASSWORD
-    //     },
+        console.log("Credentials obtained, sending message...");
 
-    //     host: "smtp.gmail.com",
-    //     port: 587,
-    //     secure: false
-    //   });
+        // Create a SMTP transporter object
+        let transporter = nodemailer.createTransport({
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+            user: account.user,
+            pass: account.pass
+          }
+        });
 
-    //   // 2
-    //   let emailBody = transporter.sendMail({
-    //     from: '"Pixo test 👻" <jaroslaw.filipiak@25wat.com>', // sender address
-    //     to: "info@j-filipiak.pl", // list of receivers
-    //     subject: "Hello ✔", // Subject line
-    //     text: "Hello world?", // plain text body
-    //     html: "<b>Hello world?</b>" // html body
-    //   });
+        // Message object
+        let message = {
+          from: "Sender Name <sender@example.com>",
+          to: "Recipient <recipient@example.com>",
+          subject: "Nodemailer is unicode friendly ✔",
+          text: "Hello to myself!",
+          html: "<p><b>Hello</b> to myself!</p>"
+        };
 
-    //   // 3
+        transporter.sendMail(message, (err, info) => {
+          if (err) {
+            console.log("Error occurred. " + err.message);
+            return process.exit(1);
+          }
 
-    //   transporter.sendMail(emailBody, function(err, data) {
-    //     if (err) {
-    //       console.log("err//email", err);
-    //     } else {
-    //       console.log("email send");
-    //     }
-    //   });
-    // },
-    // testMailer() {
-    //   // Use at least Nodemailer v4.1.0
-    //   const nodemailer = require("nodemailer");
-    //   // Generate SMTP service account from ethereal.email
-    //   nodemailer.createTestAccount((err, account) => {
-    //     if (err) {
-    //       console.error("Failed to create a testing account. " + err.message);
-    //       return process.exit(1);
-    //     }
-
-    //     console.log("Credentials obtained, sending message...");
-
-    //     // Create a SMTP transporter object
-    //     let transporter = nodemailer.createTransport({
-    //       host: account.smtp.host,
-    //       port: account.smtp.port,
-    //       secure: account.smtp.secure,
-    //       auth: {
-    //         user: account.user,
-    //         pass: account.pass
-    //       }
-    //     });
-
-    //     // Message object
-    //     let message = {
-    //       from: "Sender Name <sender@example.com>",
-    //       to: "Recipient <recipient@example.com>",
-    //       subject: "Nodemailer is unicode friendly ✔",
-    //       text: "Hello to myself!",
-    //       html: "<p><b>Hello</b> to myself!</p>"
-    //     };
-
-    //     transporter.sendMail(message, (err, info) => {
-    //       if (err) {
-    //         console.log("Error occurred. " + err.message);
-    //         return process.exit(1);
-    //       }
-
-    //       console.log("Message sent: %s", info.messageId);
-    //       // Preview only available when sending through an Ethereal account
-    //       console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    //     });
-    //   });
-    // }
-
-    postMailer() {
-      axios
-        .post("/api/mailer", this.pdfUrlPath)
-        .then(data => {})
-        .catch(error => {});
+          console.log("Message sent: %s", info.messageId);
+          // Preview only available when sending through an Ethereal account
+          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        });
+      });
     }
   }
 };
