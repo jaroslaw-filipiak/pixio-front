6134da69a802e2feb20cfa6135bf45eabac50d14
jaroslaw-filipiak <info@j-filipiak.pl>
2021-03-31 06:29:03 +0200

    default user agent

--- nuxt.config.js
+++ nuxt.config.js
@@ -125,6 +125,11 @@ export default {
     "@nuxtjs/apollo",
     "@nuxtjs/device"
   ],
+  device: {
+    refreshOnResize: true,
+    defaultUserAgent:
+      "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Mobile Safari/537.36"
+  },
 
   axios: {
     // proxy: true
