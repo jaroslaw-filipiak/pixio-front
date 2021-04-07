b539976c5633084b6b4e50907e6f264f245b4f45
lukaszdabrzalski <dab.lukasz@25wat.com>
2021-04-08 01:18:47 +0200

    page instagram - add feed preview

--- components/HomePage/Slides/Instagram.vue
+++ components/HomePage/Slides/Instagram.vue
@@ -2,70 +2,85 @@
   <div class="section section-instagram" id="section-instagram">
     <div class="slide">
       <div class="instagram-heading">Follow us on Instagram</div>
-      <div id="instafeed"></div>
+      <div class="instagram-feed" id="instafeed"></div>
     </div>
   </div>
 </template>
 
 <script>
-import Instafeed from "instafeed.js";
+import Instafeed from "@/plugins/instafeed.js";
 
 export default {
   mounted() {
     var feed = new Instafeed({
-      userId: "2041821631",
-      accessToken:
-        "IGQVJVMDJad2VjeFhJdnVFQVBSaDRBWk14djk5aVQ2MGNTZAVJwWkJWa01qbGRqRFZAZAblRPM0EzTmxFTWc2QmhyVDZAnWUN2b05NX2lFQzdRR2tLb3FwYlBzdzdXTmZAvWGM2dXh2ZATQ4RXg5aFNsU1pZAOAZDZD"
+      accessToken: `${process.env.INSTAGRAM_TOKEN}`,
+      limit: 12,
     });
     feed.run();
   },
-  methods: {}
 };
 </script>
 
 <style lang="scss">
-#instagram-feed1 {
-  width: 60vw;
-  @include xxl-min {
-    width: 80vw;
-  }
-}
+  #section-instagram {
+    .fp-controlArrow {
+      display: none !important;
+    }
 
-#section-instagram {
-  .fp-controlArrow {
-    display: none !important;
-  }
+    .fp-prev .fp-next {
+      display: none !important;
+    }
 
-  .fp-prev .fp-next {
-    display: none !important;
-  }
+    .fp-tableCell {
+      display: flex !important;
+      flex-direction: column !important;
+      align-items: center !important;
+      justify-content: space-between;
+      height: 100vh !important;
+      padding: 30px 10px;
 
-  .fp-tableCell {
-    display: flex !important;
-    flex-direction: column !important;
-    align-items: center !important;
-    justify-content: space-between !important;
-    height: 100vh !important;
-    padding: 60px 100px;
-    @include xs {
-      height: 90vh !important;
+      @include xs {
+        height: 90vh !important;
+      }
+      @include md-min {
+        padding: 60px 100px;
+      }
+      @include lg-min {
+        justify-content: flex-start;
+      }
     }
   }
-}
-.instagram-heading {
-  font-size: 24px;
-  text-align: center;
-  color: #fff;
-}
 
-.instagram-feed {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-}
-.instagram-post {
-  width: 200px;
-  height: 200px;
-}
+  .instagram-heading {
+    font-size: 24px;
+    text-align: center;
+    color: #fff;
+    margin-bottom: 20px;
+  }
+
+  .instagram-feed {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    a {
+      width: 200px;
+      height: 200px;
+      overflow: hidden;
+
+      @include lg-min {
+        width: 300px;
+        height: 300px;
+      }
+
+      img {
+        min-height: 100%;
+        width: 100%;
+        max-width: unset;
+        object-fit: cover;
+      }
+    }
+  }
 </style>
