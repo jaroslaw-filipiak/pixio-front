b1de9e5353a0c954075db782517ff4948363b28c
jaroslaw-filipiak <info@j-filipiak.pl>
2021-04-08 14:34:31 +0200

    FULLPAGE_KEY in env

--- components/HomePage/FullPageHome.vue
+++ components/HomePage/FullPageHome.vue
@@ -60,7 +60,7 @@ export default {
   data() {
     return {
       options: {
-        licenseKey: "YOUR_KEY_HEERE",
+        licenseKey: process.env.FULLPAGE_KEY,
         menu: "#side-menu",
         anchors: [
           "home",
