64a8538ded3c38d4f69336a512a25fc072aa71aa
jaroslaw-filipiak <info@j-filipiak.pl>
2021-04-08 07:45:25 +0200

    instagram styles

--- components/HomePage/Slides/Instagram.vue
+++ components/HomePage/Slides/Instagram.vue
@@ -14,73 +14,88 @@ export default {
   mounted() {
     var feed = new Instafeed({
       accessToken: `${process.env.INSTAGRAM_TOKEN}`,
-      limit: 12,
+      limit: 10
     });
     feed.run();
-  },
+  }
 };
 </script>
 
 <style lang="scss">
-  #section-instagram {
-    .fp-controlArrow {
-      display: none !important;
-    }
-
-    .fp-prev .fp-next {
-      display: none !important;
-    }
-
-    .fp-tableCell {
-      display: flex !important;
-      flex-direction: column !important;
-      align-items: center !important;
-      justify-content: space-between;
-      height: 100vh !important;
-      padding: 30px 10px;
-
-      @include xs {
-        height: 90vh !important;
-      }
-      @include md-min {
-        padding: 60px 100px;
-      }
-      @include lg-min {
-        justify-content: flex-start;
-      }
-    }
+#instafeed {
+  @include lg-min {
+    width: calc(190px * 5);
   }
 
-  .instagram-heading {
-    font-size: 24px;
-    text-align: center;
-    color: #fff;
-    margin-bottom: 20px;
-  }
-
-  .instagram-feed {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-    gap: 10px;
-
-    a {
-      width: 200px;
-      height: 200px;
-      overflow: hidden;
-
-      @include lg-min {
-        width: 300px;
-        height: 300px;
-      }
-
-      img {
-        min-height: 100%;
-        width: 100%;
-        max-width: unset;
-        object-fit: cover;
-      }
+  a {
+    width: 120px;
+    height: 120px;
+    @include lg-min {
+      width: 182px;
+      height: 182px;
     }
   }
+}
+
+#section-instagram {
+  .fp-controlArrow {
+    display: none !important;
+  }
+
+  .fp-prev .fp-next {
+    display: none !important;
+  }
+
+  .fp-tableCell {
+    display: flex !important;
+    flex-direction: column !important;
+    align-items: center !important;
+    justify-content: flex-start;
+    height: 100vh !important;
+    padding: 30px 10px;
+
+    @include xs {
+      height: 90vh !important;
+    }
+    @include md-min {
+      padding: 60px 100px;
+    }
+    @include lg-min {
+      justify-content: center;
+    }
+  }
+}
+
+.instagram-heading {
+  font-size: 24px;
+  text-align: center;
+  color: #fff;
+  margin-bottom: 20px;
+}
+
+.instagram-feed {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+
+  a {
+    width: 200px;
+    height: 200px;
+    overflow: hidden;
+
+    @include lg-min {
+      width: 300px;
+      height: 300px;
+    }
+
+    img {
+      min-height: 100%;
+      width: 100%;
+      max-width: unset;
+      object-fit: cover;
+    }
+  }
+}
 </style>
